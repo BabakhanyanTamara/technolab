1dcb0ee00b3c90302e0ba31a8e0cd43170dc96de
BabakhanyanTamara <105594366+BabakhanyanTamara@users.noreply.github.com>
2022-06-03 16:11:16 +0400

    Add files via upload

---
+++ recursion.js
@@ -0,0 +1,10 @@
+var isPalindrome = function (str){
+    if (str.length === 0 || str.length === 1){
+        return true;
+    }   
+   if (str[0] !== str[str.length -1]){
+       return false;
+   }
+   return isPalindrome(str.substring(1,str.length -1));
+}; 
+console.log(isPalindrome("katak"));
